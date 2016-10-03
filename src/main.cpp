ac90bc5eee8c59d2ff3017ca1348deb392d658d2
ShenyaoKe <akika@tamu.edu>
2016-10-03 01:18:40 -0500

    add support for modeling process.

--- src/main.cpp
+++ src/main.cpp
@@ -1,123 +1,5 @@
-#include <GL/glew.h> /* include GLEW and new version of GL on Windows */
-#include <GL/glfw3.h> /* GLFW helper library */
-#include "GLSLProgram.h"
-#include "OGLViewer.h"
-#include "SubdMesh.h"
 
-static uint32_t win_width = 640;
-static uint32_t win_height = 480;
-
-unique_ptr<perspCamera> view_cam = make_unique<perspCamera>(
-	Point3f(10, 6, 10), Point3f(0, 0, 0), Vector3f(0, 1, 0),
-	win_width / static_cast<Float>(win_height));
-
-SubdMesh model_mesh("scene/obj/torus_low.obj");
-vector<GLfloat> model_verts;// vertices vbo
-vector<GLuint> model_idx;// Normal coordinates vbo
-BufferTrait patchTrats;
-GLuint model_vert_vbo, model_ibo, model_vao;
-unique_ptr<GLSLProgram> model_shader;
-
-const GLubyte* renderer;
-const GLubyte* version;
-
-bool draw_wireframe = true;
-
-void bindMesh()
-{
-	glDeleteBuffers(1, &model_vert_vbo);
-	glDeleteBuffers(1, &model_ibo);
-	glDeleteVertexArrays(1, &model_vao);
-
-	glCreateBuffers(1, &model_vert_vbo);
-	glNamedBufferData(model_vert_vbo, sizeof(GLfloat) * model_verts.size(), &model_verts[0], GL_STATIC_DRAW);
-
-	// IBO
-	glCreateBuffers(1, &model_ibo);
-	glNamedBufferData(model_ibo, sizeof(GLuint) * model_idx.size(), &model_idx[0], GL_STATIC_DRAW);
-
-	// VAO
-	glCreateVertexArrays(1, &model_vao);
-	glEnableVertexArrayAttrib(model_vao, 0);
-
-	// Setup the formats
-	glVertexArrayAttribFormat(model_vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
-	glVertexArrayVertexBuffer(model_vao, 0, model_vert_vbo, 0, sizeof(GLfloat) * 3);
-	glVertexArrayAttribBinding(model_vao, 0, 0);
-
-	glVertexArrayElementBuffer(model_vao, model_ibo);
-}
-
-
-void bindPatch()
-{
-	glDeleteBuffers(1, &model_vert_vbo);
-	glDeleteBuffers(1, &model_ibo);
-	glDeleteVertexArrays(1, &model_vao);
-
-	glCreateBuffers(1, &model_vert_vbo);
-	glNamedBufferData(model_vert_vbo, patchTrats.size, patchTrats.data, GL_STATIC_DRAW);
-
-	// IBO
-	/*glCreateBuffers(1, &model_ibo);
-	glNamedBufferData(model_ibo, sizeof(GLuint) * model_idx.size(), &model_idx[0], GL_STATIC_DRAW);*/
-
-	// VAO
-	glCreateVertexArrays(1, &model_vao);
-	glEnableVertexArrayAttrib(model_vao, 0);
-
-	// Setup the formats
-	glVertexArrayAttribFormat(model_vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
-	glVertexArrayVertexBuffer(model_vao, 0, model_vert_vbo, patchTrats.offset, patchTrats.stride);
-	glVertexArrayAttribBinding(model_vao, 0, 0);
-}
-
-void initGL()
-{
-	renderer = glGetString(GL_RENDERER); /* get renderer string */
-	version = glGetString(GL_VERSION); /* version as a string */
-	printf("Renderer: %s\n", renderer);
-	printf("OpenGL version supported %s\n", version);
-
-	glewExperimental = GL_TRUE;
-	glewInit();
-
-	/* geometry to use. these are 3 xyz points (9 floats total) to make a triangle */
-	model_shader = make_unique<GLSLProgram>(
-		"shaders/quad_vs.glsl", "shaders/quad_fs.glsl", "shaders/quad_gs.glsl");
-
-	model_mesh.exportIndexedVBO(&model_verts, nullptr, nullptr, &model_idx);
-	bindMesh();
-
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
-}
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	if (key == GLFW_KEY_F && action == GLFW_PRESS)
-	{
-		view_cam.reset(new perspCamera(
-			Point3f(10, 6, 10), Point3f(0, 0, 0), Vector3f(0, 1, 0),
-			win_width / static_cast<Float>(win_height)));
-	}
-}
-
-void window_resize_callback(GLFWwindow* window, int width, int height)
-{
-	win_width = width;
-	win_height = height;
-	view_cam->resizeViewport(win_width / static_cast<Float>(win_height));
-	glViewport(0, 0, win_width, win_height);
-}
-
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
-{
-	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
-	{
-		// Do sth
-	}
-}
+#include "main.h"
 
 int main()
 {
@@ -142,7 +24,13 @@ int main()
 	glfwSetKeyCallback(window, key_callback);
 	glfwSetWindowSizeCallback(window, window_resize_callback);
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
+		
+
+	GLFWcursor* cursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+	if (cursor) glfwSetCursor(window, cursor);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
 	/* tell GL to only draw onto a pixel if the shape is closer to the viewer
 	than anything already drawn at that pixel */
 	
@@ -178,6 +66,7 @@ int main()
 		glfwSwapBuffers(window);
 	}
 
+	glfwDestroyCursor(cursor);
 	/* close GL context and any other GLFW resources */
 	glfwTerminate();
 	return 0;
---
+++ src/main.h
@@ -0,0 +1,160 @@
+#pragma once
+#include <GL/glew.h> /* include GLEW and new version of GL on Windows */
+#include <GL/glfw3.h> /* GLFW helper library */
+#include "GLSLProgram.h"
+#include "OGLViewer.h"
+#include "SubdMesh.h"
+
+
+static uint32_t win_width = 640;
+static uint32_t win_height = 480;
+static double cursor_last_x, cursor_last_y;
+
+unique_ptr<perspCamera> view_cam = make_unique<perspCamera>(
+	Point3f(10, 6, 10), Point3f(0, 0, 0), Vector3f(0, 1, 0),
+	win_width / static_cast<Float>(win_height));
+
+SubdMesh model_mesh("scene/obj/torus_low.obj");
+vector<GLfloat> model_verts;// vertices vbo
+vector<GLuint> model_idx;// Normal coordinates vbo
+BufferTrait patchTrats;
+GLuint model_vert_vbo, model_ibo, model_vao;
+GLuint patch_vert_vbo, patch_vao;
+unique_ptr<GLSLProgram> model_shader;
+
+const GLubyte* renderer;
+const GLubyte* version;
+
+bool draw_wireframe = true;
+bool move_camera = false;
+bool zoom_camera = false;
+
+void bindMesh()
+{
+	glDeleteBuffers(1, &model_vert_vbo);
+	glDeleteBuffers(1, &model_ibo);
+	glDeleteVertexArrays(1, &model_vao);
+
+	glCreateBuffers(1, &model_vert_vbo);
+	glNamedBufferData(model_vert_vbo, sizeof(GLfloat) * model_verts.size(), &model_verts[0], GL_STATIC_DRAW);
+
+	// IBO
+	glCreateBuffers(1, &model_ibo);
+	glNamedBufferData(model_ibo, sizeof(GLuint) * model_idx.size(), &model_idx[0], GL_STATIC_DRAW);
+
+	// VAO
+	glCreateVertexArrays(1, &model_vao);
+	glEnableVertexArrayAttrib(model_vao, 0);
+
+	// Setup the formats
+	glVertexArrayAttribFormat(model_vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
+	glVertexArrayVertexBuffer(model_vao, 0, model_vert_vbo, 0, sizeof(GLfloat) * 3);
+	glVertexArrayAttribBinding(model_vao, 0, 0);
+
+	glVertexArrayElementBuffer(model_vao, model_ibo);
+}
+
+
+void bindPatch()
+{
+	glDeleteBuffers(1, &patch_vert_vbo);
+	glDeleteVertexArrays(1, &patch_vao);
+
+	glCreateBuffers(1, &patch_vert_vbo);
+	glNamedBufferData(patch_vert_vbo, patchTrats.size, patchTrats.data, GL_STATIC_DRAW);
+
+	// IBO
+	/*glCreateBuffers(1, &model_ibo);
+	glNamedBufferData(model_ibo, sizeof(GLuint) * model_idx.size(), &model_idx[0], GL_STATIC_DRAW);*/
+
+	// VAO
+	glCreateVertexArrays(1, &patch_vao);
+	glEnableVertexArrayAttrib(patch_vao, 0);
+
+	// Setup the formats
+	glVertexArrayAttribFormat(patch_vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
+	glVertexArrayVertexBuffer(patch_vao, 0, model_vert_vbo, patchTrats.offset, patchTrats.stride);
+	glVertexArrayAttribBinding(patch_vao, 0, 0);
+}
+
+void initGL()
+{
+	renderer = glGetString(GL_RENDERER); /* get renderer string */
+	version = glGetString(GL_VERSION); /* version as a string */
+	printf("Renderer: %s\n", renderer);
+	printf("OpenGL version supported %s\n", version);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	/* geometry to use. these are 3 xyz points (9 floats total) to make a triangle */
+	model_shader = make_unique<GLSLProgram>(
+		"shaders/quad_vs.glsl", "shaders/quad_fs.glsl", "shaders/quad_gs.glsl");
+
+	model_mesh.exportIndexedVBO(&model_verts, nullptr, nullptr, &model_idx);
+	bindMesh();
+	model_mesh.getPatch(patchTrats);
+	bindPatch();
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_F && action == GLFW_PRESS)
+	{
+		view_cam.reset(new perspCamera(
+			Point3f(10, 6, 10), Point3f(0, 0, 0), Vector3f(0, 1, 0),
+			win_width / static_cast<Float>(win_height)));
+	}
+}
+
+void window_resize_callback(GLFWwindow* window, int width, int height)
+{
+	win_width = width;
+	win_height = height;
+	view_cam->resizeViewport(win_width / static_cast<Float>(win_height));
+	glViewport(0, 0, win_width, win_height);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+	{
+		// Do sth
+		zoom_camera = true;
+	}
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		glfwGetCursorPos(window, &cursor_last_x, &cursor_last_y);
+		move_camera = true;
+	}
+	if (action == GLFW_RELEASE)
+	{
+		move_camera = zoom_camera = false;
+	}
+}
+
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	double dx = xpos - cursor_last_x;
+	double dy = ypos - cursor_last_y;
+
+	if (move_camera)
+	{
+		view_cam->rotate(dy * 0.25f, -dx * 0.25f, 0.0);
+	}
+	else if (zoom_camera && dx != cursor_last_x)// zooming
+	{
+		view_cam->zoom(0.0, 0.0, dx * 0.05);
+	}
+
+	cursor_last_x = xpos;
+	cursor_last_y = ypos;
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	view_cam->zoom(0, 0, yoffset);
+}
